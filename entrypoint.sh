8e20331bc16686230c51eff52214e8e0b40461cc
Lida He <lida.he@emc.com>
2017-06-20 19:47:11 -0400

    change env name

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,13 @@ set -e
 #     - 10.1.1.2
 #
 # Elasticsearch hosts are passed in as a comma separated list, 10.1.1.1,10.1.1.2
-ELASTICSEARCH_HOSTS=${ELASTICSEARCH_HOSTS:-localhost}
+ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-localhost}
 ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-9200}
 RETENTION_DAYS=${RETENTION_DAYS:-45}
 
-hosts="\    - ""`echo $ELASTICSEARCH_HOSTS | sed -e 's/,/\\\n    - /g'`"
-sed -i "/hosts:/a ${hosts}" /curator/config/curator.yml
+#hosts="\    - ""`echo $ELASTICSEARCH_HOSTS | sed -e 's/,/\\\n    - /g'`"
+#sed -i "/hosts:/a ${hosts}" /curator/config/curator.yml
+sed -i "s/hosts: .*$/hosts: ${ELASTICSEARCH_HOST}/" /curator/config/curator.yml
 sed -i "s/port: .*$/port: ${ELASTICSEARCH_PORT}/" /curator/config/curator.yml
 sed -i "s/unit_count: .*$/unit_count: ${RETENTION_DAYS}/" /curator/config/action.yml
 
