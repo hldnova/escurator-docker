0c4d063c2cbc5dab124fd049e5c4c36d293d96dd
Lida He <lida.he@emc.com>
2017-06-20 16:59:22 -0400

    update env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ set -e
 #
 # Elasticsearch hosts are passed in as a comma separated list, 10.1.1.1,10.1.1.2
 ELASTICSEARCH_HOSTS=${ELASTICSEARCH_HOSTS:-localhost}
-ELASTICSEARCH_HOSTS=${ELASTICSEARCH_PORT:-9200}
+ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-9200}
 RETENTION_DAYS=${RETENTION_DAYS:-45}
 
 hosts="\    - ""`echo $ELASTICSEARCH_HOSTS | sed -e 's/,/\\\n    - /g'`"
