b808fbf3083cadea5c9b7a5d607c12010581f8c2
Lida He <lida.he@emc.com>
2017-06-20 15:13:47 -0400

    update configuration directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ ELASTICSEARCH_HOSTS=${ELASTICSEARCH_PORT:-9200}
 RETENTION_DAYS=${RETENTION_DAYS:-45}
 
 hosts="\    - ""`echo $ELASTICSEARCH_HOSTS | sed -e 's/,/\\\n    - /g'`"
-sed -i "/hosts:/a ${hosts}" curator.yml
-sed -i "s/port: .*$/port: ${ELASTICSEARCH_PORT}/" curator.yml
-sed -i "s/unit_count: .*$/unit_count: ${RETENTION_DAYS}/" action.yml
+sed -i "/hosts:/a ${hosts}" /curator/config/curator.yml
+sed -i "s/port: .*$/port: ${ELASTICSEARCH_PORT}/" /curator/config/curator.yml
+sed -i "s/unit_count: .*$/unit_count: ${RETENTION_DAYS}/" /curator/config/action.yml
 
 exec "$@"
